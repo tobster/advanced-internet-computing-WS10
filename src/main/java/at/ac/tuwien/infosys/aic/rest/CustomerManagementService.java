e535ec8fd4dc0a8e4c336f07eed19a0d84886ba1
Christoph Derndorfer <e0425826@student.tuwien.ac.at>
2010-11-10 16:28:16 +0100

    added skeleton of CustomerManagementService.java for REST assignment

---
+++ src/main/java/at/ac/tuwien/infosys/aic/rest/CustomerManagementService.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package at.ac.tuwien.infosys.aic.rest;
+
+public class CustomerManagementService {
+
+    //handleGet -> Customer Info holen
+    //handlePost -> Customer aktualisieren
+    //handlePut -> Customer hinzufuegen
+    //handleDelete -> Customer loeschen
+
+    //notify (customer and message, as string)
+    //update_account (customer, changed value as BigDecimal). update account just takes the customer and adds the changedValue parameter to the customer's open balance eld.
+
+}
