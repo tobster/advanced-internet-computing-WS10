c8250739b0d470bf9709e17e4596ea60b84de0a7
Michael Smolle <michael.smolle@tuwien.ac.at>
2010-11-11 22:51:25 +0100

    added first method to CustomerManagementServiceWrapperImpl

--- src/main/java/at/ac/tuwien/infosys/aic/soap/CustomerManagementServiceWrapperImpl.java
+++ src/main/java/at/ac/tuwien/infosys/aic/soap/CustomerManagementServiceWrapperImpl.java
@@ -5,7 +5,9 @@
 
 package at.ac.tuwien.infosys.aic.soap;
 
-import javax.jws.WebService;
+import javax.ws.rs.core.UriBuilder;
+import java.net.URI;
+import org.apache.cxf.jaxrs.client.JAXRSClientFactory;
 import at.ac.tuwien.infosys.aic.model.Customer;
 import static at.ac.tuwien.infosys.aic.Constants.*;
 
@@ -14,8 +16,22 @@ public class CustomerManagementServiceWrapperImpl implements CustomerManagementS
 
     @Override
     public Customer get(String id) {
-        throw new UnsupportedOperationException("Not supported yet.");
+
+        URI uri = UriBuilder.fromUri(CUSTOMERMANAGEMENT).path("Customer").path(id).build();
+        Customer customer = JAXRSClientFactory.create( uri.toString() , Customer.class);  
+        return customer;
+        
     }
+
+//BookStore store = JAXRSClientFactory.create("http://bookstore.com", BookStore.class);
+// (1) remote GET call to http://bookstore.com/bookstore
+//Books books = store.getAllBooks();
+// (2) no remote call
+//BookResource subresource = store.getBookSubresource(1);
+// {3} remote GET call to http://bookstore.com/bookstore/1
+//Book b = subresource.getDescription();
+
+
 
     @Override
     public void put(Customer customer) {
