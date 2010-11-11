4d3d0910545f39b6c5e31ab4f27de2ddb4337b18
Tobias Hochwallner <tobster@gmx.at>
2010-11-11 18:16:49 +0100

    activated logginginterceptors + cleanup

--- src/main/java/at/ac/tuwien/infosys/aic/server/Server.java
+++ src/main/java/at/ac/tuwien/infosys/aic/server/Server.java
@@ -22,33 +22,33 @@ public class Server {
     private List<org.apache.cxf.endpoint.Server> servers = new LinkedList<org.apache.cxf.endpoint.Server>();
     private LoggingInInterceptor loggingInInterceptor = new LoggingInInterceptor();
     private LoggingOutInterceptor loggingOutInterceptor = new LoggingOutInterceptor();
-    JaxWsServerFactoryBean svrFactory;
 
     public Server() {
 
         this.addShutdownHook();
         // SOAP
-        startService(svrFactory, ShippingService.class, SHIPPINGADDRESS, new ShippingServiceImpl());
-        startService(svrFactory, SupplierService.class, SUPPLIER1ADDRESS, new SupplierImpl());
-        startService(svrFactory, SupplierService.class, SUPPLIER2ADDRESS, new SupplierImpl());
-        startService(svrFactory, WarehouseService.class, WAREHOUSEADDRESS, new WarehouseServiceImpl());
-        startService(svrFactory, ServiceRegistry.class, REGISTRYADDRESS, new ServiceRegistryImpl());
-        //startService(svrFactory, CustomerManagementService.class, CUSTOMERMANAGEMENT, new ServiceRegistryImpl());
+        startService(ShippingService.class, SHIPPINGADDRESS, new ShippingServiceImpl());
+        startService(SupplierService.class, SUPPLIER1ADDRESS, new SupplierImpl());
+        startService(SupplierService.class, SUPPLIER2ADDRESS, new SupplierImpl());
+        startService(WarehouseService.class, WAREHOUSEADDRESS, new WarehouseServiceImpl());
+        startService(ServiceRegistry.class, REGISTRYADDRESS, new ServiceRegistryImpl());
 
         //REST
         JAXRSServerFactoryBean sf = new JAXRSServerFactoryBean();
         sf.setAddress(CUSTOMERMANAGEMENT);
         sf.setServiceBeans(new CustomerManagementService());
+        sf.getInInterceptors().add(loggingInInterceptor);
+        sf.getOutInterceptors().add(loggingOutInterceptor);
         servers.add(sf.create());
     }
 
-    private void startService(JaxWsServerFactoryBean svrFactory, Class iface, String address, Object implementation) {
-        svrFactory = new JaxWsServerFactoryBean();
+    private void startService(Class iface, String address, Object implementation) {
+        JaxWsServerFactoryBean svrFactory = new JaxWsServerFactoryBean();
         svrFactory.setServiceClass(iface);
         svrFactory.setAddress(address);
         svrFactory.setServiceBean(implementation);
-        //svrFactory.getInInterceptors().add(loggingInInterceptor);
-        //svrFactory.getOutInterceptors().add(loggingOutInterceptor);
+        svrFactory.getInInterceptors().add(loggingInInterceptor);
+        svrFactory.getOutInterceptors().add(loggingOutInterceptor);
         servers.add(svrFactory.create());
     }
 
